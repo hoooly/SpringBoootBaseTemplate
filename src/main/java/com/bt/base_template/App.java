19a35506a64f5a53f7238accca8c62a60f4dfb7e
zhiwei.liang(IT/SN) <zhiwei.liang@ninestar.local>
2019-12-13 10:41:50 +0800

    first commit

---
+++ src/main/java/com/bt/base_template/App.java
@@ -0,0 +1,13 @@
+package com.bt.base_template;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
